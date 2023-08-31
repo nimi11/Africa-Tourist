261fdb0a2431ca2089bea6e87e1fef07b5818784
Toluwanimi Sonuga <toluwanimi.sonuga@kibo.school>
2023-08-31 19:29:36 +0100

    7

--- script.js
+++ script.js
@@ -5,11 +5,20 @@ document.addEventListener("DOMContentLoaded", function(){
     const g2= document.getElementById("g-2");
     const g3 = document.getElementById("g-3");
     const g4 = document.getElementById("g-4");
-    const g5 = document.getElementById("g-5")
+    const g5 = document.getElementById("g-5");
+    const g6 = document.getElementById("g-6")
+    const g7 = document.getElementById("g-7")
+    const g8 = document.getElementById("g-8")
+    const g9 = document.getElementById("g-9")
     const firstSite = document.getElementById("first-site")
     const secondSite = document.getElementById("second-site")
     const thirdSite = document.getElementById("third-site")
     const fourthSite = document.getElementById("fourth-site")
+    const fifthSite = document.getElementById("fifth-site")
+    const sixthSite = document.getElementById("sixth-site")
+    const seventhSite = document.getElementById("seventh-site")
+    const eightSite = document.getElementById("eighth-site")
+    const ninthSite = document.getElementById("ninth-site")
     const nextButton = document.getElementById("next")
     const prevButton = document.getElementById("previous")
    const prevArrow = document.getElementById("prev-arrow")
@@ -47,7 +56,7 @@ document.addEventListener("DOMContentLoaded", function(){
         const nextItemIndex = (currentIndex + i) % galleryItems.length;
         galleryItems[nextItemIndex].classList.remove('hidden');
     }
-});
+    });
 
 
     g1.addEventListener("click", function() {
@@ -60,6 +69,11 @@ document.addEventListener("DOMContentLoaded", function(){
         secondSite.classList.add("hidden")
         thirdSite.classList.add("hidden")
         fourthSite.classList.add("hidden")
+        fifthSite.classList.add("hidden")
+        sixthSite.classList.add("hidden")
+        seventhSite.classList.add("hidden")
+        eightSite.classList.add("hidden")
+        ninthSite.classList.add("hidden")
     });
 
     g2.addEventListener("click", function(){
@@ -71,6 +85,11 @@ document.addEventListener("DOMContentLoaded", function(){
         firstSite.classList.add("hidden")
         thirdSite.classList.add("hidden")
         fourthSite.classList.add("hidden")
+        fifthSite.classList.add("hidden")
+        sixthSite.classList.add("hidden")
+        seventhSite.classList.add("hidden")
+        eightSite.classList.add("hidden")
+        ninthSite.classList.add("hidden")
     })
     g3.addEventListener("click", function(){
         thirdSite.classList.remove("hidden");
@@ -81,6 +100,11 @@ document.addEventListener("DOMContentLoaded", function(){
         firstSite.classList.add("hidden")
         secondSite.classList.add("hidden")
         fourthSite.classList.add("hidden")
+        fifthSite.classList.add("hidden")
+        sixthSite.classList.add("hidden")
+        seventhSite.classList.add("hidden")
+        eightSite.classList.add("hidden")
+        ninthSite.classList.add("hidden")
     })
     g4.addEventListener("click", function(){
         fourthSite.classList.remove("hidden")
@@ -91,8 +115,91 @@ document.addEventListener("DOMContentLoaded", function(){
         firstSite.classList.add("hidden")
         secondSite.classList.add("hidden")
         thirdSite.classList.add("hidden")
-        
+        fifthSite.classList.add("hidden")
+        sixthSite.classList.add("hidden")
+        seventhSite.classList.add("hidden")
+        eightSite.classList.add("hidden")
+        ninthSite.classList.add("hidden")
     })
+    g5.addEventListener("click", function(){
+        fifthSite.classList.remove("hidden")
+        fifthSite.scrollIntoView({behaviour:"smooth"})
+        nextButton.classList.remove("hidden");
+        prevButton.classList.remove("hidden");
+        g=5
+        firstSite.classList.add("hidden")
+        secondSite.classList.add("hidden")
+        thirdSite.classList.add("hidden")
+        fourthSite.classList.add("hidden")
+        sixthSite.classList.add("hidden")
+        seventhSite.classList.add("hidden")
+        eightSite.classList.add("hidden")
+        ninthSite.classList.add("hidden")
+
+    })
+    g6.addEventListener("click",function(){
+        sixthSite.classList.remove("hidden")
+        sixthSite.scrollIntoView({behavior:"smooth"})
+        nextButton.classList.remove("hidden");
+        prevButton.classList.remove("hidden");
+        g=6
+        firstSite.classList.add("hidden")
+        secondSite.classList.add("hidden")
+        thirdSite.classList.add("hidden")
+        fourthSite.classList.add("hidden")
+        fifthSite.classList.add("hidden")
+        seventhSite.classList.add("hidden")
+        eightSite.classList.add("hidden")
+        ninthSite.classList.add("hidden")
+
+    })
+    g7.addEventListener("click",function(){
+        seventhSite.classList.remove("hidden")
+        seventhSite.scrollIntoView({behavior:"smooth"})
+        nextButton.classList.remove("hidden");
+        prevButton.classList.remove("hidden");
+        g=7
+        firstSite.classList.add("hidden")
+        secondSite.classList.add("hidden")
+        thirdSite.classList.add("hidden")
+        fourthSite.classList.add("hidden")
+        fifthSite.classList.add("hidden")
+        sixthSite.classList.add("hidden")
+        eightSite.classList.add("hidden")
+        ninthSite.classList.add("hidden")
+    })
+    g8.addEventListener("click",function(){
+        eightSite.classList.remove("hidden")
+        eightSite.scrollIntoView({behavior:"smooth"})
+        nextButton.classList.remove("hidden");
+        prevButton.classList.remove("hidden");
+        g=8
+        firstSite.classList.add("hidden")
+        secondSite.classList.add("hidden")
+        thirdSite.classList.add("hidden")
+        fourthSite.classList.add("hidden")
+        fifthSite.classList.add("hidden")
+        sixthSite.classList.add("hidden")
+        seventhSite.classList.add("hidden")
+        ninthSite.classList.add("hidden")
+
+    })
+    g9.addEventListener("click", function(){
+        ninthSite.classList.remove("hidden")
+        ninthSite.scrollIntoView({behavior:"smooth"})
+        nextButton.classList.remove("hidden");
+        prevButton.classList.remove("hidden");
+        g=9
+        firstSite.classList.add("hidden")
+        secondSite.classList.add("hidden")
+        thirdSite.classList.add("hidden")
+        fourthSite.classList.add("hidden")
+        fifthSite.classList.add("hidden")
+        sixthSite.classList.add("hidden")
+        seventhSite.classList.add("hidden")
+        eightSite.classList.add("hidden")
+    })
+
 
 
 
