5d5209e031a53b496e123ae623b3f32c19236756
Toluwanimi Sonuga <toluwanimi.sonuga@kibo.school>
2023-08-31 10:16:35 +0100

    3

--- script.js
+++ script.js
@@ -16,7 +16,9 @@ document.addEventListener("DOMContentLoaded", function(){
         firstSite.scrollIntoView({ behavior: "smooth" });
         nextButton.classList.remove("hidden");
         prevButton.classList.remove("hidden");
-        g+=1
+        g=1
+        secondSite.classList.add("hidden")
+        thirdSite.classList.add("hidden")
     });
 
     g2.addEventListener("click", function(){
@@ -24,45 +26,53 @@ document.addEventListener("DOMContentLoaded", function(){
        secondSite.scrollIntoView({behavior: "smooth" }) ;
        nextButton.classList.remove("hidden");
        prevButton.classList.remove("hidden");
-       g+=2
-
+       g=2
+        firstSite.classList.add("hidden")
+        thirdSite.classList.add("hidden")
     })
     g3.addEventListener("click", function(){
         thirdSite.classList.remove("hidden");
         thirdSite.scrollIntoView({behavior:"smooth"});
         nextButton.classList.remove("hidden");
         prevButton.classList.remove("hidden");
-        g+=3
+        g=3
+        firstSite.classList.add("hidden")
+        secondSite.classList.add("hidden")
     })
 
+    
     prevButton.addEventListener("mouseover",function(){
         prevButton.style.transform ="scale(1.2)"
     })
     prevButton.addEventListener("mouseout",function(){
         prevButton.style.transform ="scale(1)"
     })
-
     prevButton.addEventListener("click",function(){
         if(g==1){
             console.log("g=1")
+            secondSite.classList.add("hidden")
             firstSite.classList.add("hidden")
             thirdSite.classList.remove("hidden");
             thirdSite.scrollIntoView({behavior:"smooth"})
             g=3
         }else if(g==2){
             console.log("g==2")
+            thirdSite.classList.add("hidden")
             secondSite.classList.add("hidden")
             firstSite.classList.remove("hidden")
             firstSite.scrollIntoView({behavior:"smooth"})
             g=1
 
         }else if(g==3){
-            console.log("g==3")
+            console.log(g)
+            firstSite.classList.add("hidden")
             thirdSite.classList.add("hidden")
             secondSite.classList.remove("hidden")
             secondSite.scrollIntoView({behavior:"smooth"})
             g=2
 
+        }else{
+            console.log(g)
         }
     })
 
@@ -75,7 +85,7 @@ document.addEventListener("DOMContentLoaded", function(){
 
     nextButton.addEventListener("click",function(){
         if (g==1){
-            console.log("g=1")
+            console.log(g)
             firstSite.classList.add("hidden");
             secondSite.classList.remove("hidden");
             secondSite.scrollIntoView({behavior:"smooth"});
@@ -94,6 +104,8 @@ document.addEventListener("DOMContentLoaded", function(){
             firstSite.classList.remove("hidden")
             firstSite.scrollIntoView({behavior:"smooth"})
             g=1
+        }else{
+            console.log(g)
         }
     })
 
